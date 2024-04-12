a3311abc2e91a8478d636cb6bde7bfa5951a462f
BiggestNoobEU <davis.simanis.rs@gmail.com>
2024-04-12 13:49:48 +0300

    added code get and post methods

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/documents', 'DocumentController@index');
+Route::get('/documents/{id}', 'DocumentController@show');
+
+Route::post('/documents', 'DocumentController@store');
