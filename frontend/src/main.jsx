e02ae29bba8d1cb7d6297f2a33e9287a2dbdffe8
BiggestNoobEU <davis.simanis.rs@gmail.com>
2024-04-12 17:21:53 +0300

    added bare minimum styling for new doc form

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './router.jsx';
-import './index.css'
+import './index.css';
+import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
